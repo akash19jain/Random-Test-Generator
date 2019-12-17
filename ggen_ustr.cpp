e8eced261531bc97020488bea17ef5698f57a6c3
AKASH JAIN <akash19jain@gmail.com>
2019-12-17 22:07:17 +0530

    GENERATING A STRING WITH UPPERCASE LETTERS

---
+++ ggen_ustr.cpp
@@ -0,0 +1,33 @@
+//GENERATING A STRING WITH UPPERCASE LETTERS
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#include <fstream>
+ofstream file;      // FILE DECLARATION
+typedef long long ll;
+
+int main()
+{
+    file.open("Test_Cases", ios::out | ios::trunc );
+    
+    ll maxsize,test;
+    maxsize=20;      //MAXSIZE OF THE STRING
+    test=10;        //NUMBER OF TEST CASES
+    
+    file<<test<<"\n";
+    srand(time(0));
+    for(ll i=0;i<test;i++)
+    {
+        ll s=rand()%maxsize+1;
+        file<<s<<"\n";   
+        
+        for(ll j=0;j<s;j++)
+        {
+            int a=rand()%26+65;
+            file<<(char)a;
+        }
+        file<<"\n";
+    }
+    file.close();
+	return 0;   
+}
